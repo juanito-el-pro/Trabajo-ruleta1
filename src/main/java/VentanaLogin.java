6a10e3de0ebcb3ed1c57e936d5b86c9b17d74a22
juan <j.manzur01@ufromail.cl>
2026-09-09 21:19:12 -0300

    Implementa interfaz de login y registro

--- src/main/java/VentanaLogin.java
+++ src/main/java/VentanaLogin.java
@@ -12,6 +12,7 @@ public class VentanaLogin {
     private final JLabel lblClave = new JLabel("Clave:");
     private final JPasswordField txtClave = new JPasswordField();
     private final JButton btnIngresar = new JButton("Ingresar");
+    private final JButton btnRegistro = new JButton("Registrarse");
     /**
      * Constructor que inicializa la ventana de inicio de sesión.
      * Configura sus componentes y eventos.
@@ -19,7 +20,7 @@ public class VentanaLogin {
     public VentanaLogin() {
 // TODO: Agregar los usuarios iniciales a la lista
 // TODO: Inicializar y configurar la ventana
-
+// Nota: Redistruibuir el constructor XDD
         USUARIOS.add(new Usuario("admin", "7777", "Administrador")); //credenciales almacenadas de usuarios para "probar el codigo"
         USUARIOS.add(new Usuario("juan", "1234", "Juan"));
 
@@ -34,12 +35,18 @@ public class VentanaLogin {
         txtClave.setBounds(150, 80, 180, 25); // texto que se escribe visualmente al lado
 
         btnIngresar.setBounds(150, 130, 100, 30);
+        btnRegistro.setBounds(140, 175, 120, 30);
 
         frame.add(lblUsuario);
         frame.add(txtUsuario);
         frame.add(lblClave);
         frame.add(txtClave);
         frame.add(btnIngresar);
+        frame.add(btnRegistro);
+
+        btnIngresar.addActionListener(e -> login());
+        btnRegistro.addActionListener(e -> abrirRegistro());
+
     }
 
     public void mostrarVentana() {
--- src/main/java/VentanaRegistro.java
+++ src/main/java/VentanaRegistro.java
@@ -1,2 +1,36 @@
+import javax.swing.*;
+
 public class VentanaRegistro {
+
+    // Objetos visuales para la ventana
+    private final JFrame frame = new JFrame("Registro - Casino Black Cat");
+
+    private final JLabel lblNombre = new JLabel("Nombre:");
+    private final JTextField txtNombre = new JTextField();
+
+    private final JLabel lblUsuario = new JLabel("Usuario:");
+    private final JTextField txtUsuario = new JTextField();
+
+    private final JLabel lblClave = new JLabel("Clave:");
+    private final JPasswordField txtClave = new JPasswordField();
+
+    private final JButton btnRegistrar = new JButton("Registrar");
+
+    public VentanaRegistro() {
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        lblNombre.setBounds(50, 40, 100, 25);
+        txtNombre.setBounds(150, 40, 180, 25);
+
+        lblUsuario.setBounds(50, 80, 100, 25);
+        txtUsuario.setBounds(150, 80, 180, 25);
+
+        lblClave.setBounds(50, 120, 100, 25);
+        txtClave.setBounds(150, 120, 180, 25);
+
+        btnRegistrar.setBounds(140, 170, 120, 30);
+    }
 }
+
+
